2f566d150505582b81f5a116b3f1302110111be1
Legolas <057@live.com>
2020-05-20 00:15:13 -0700

    Initial Commit

---
+++ CS193P-Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CS193P-Memorize
+//
+//  Created by Legolas on 5/20/20.
+//  Copyright © 2020 Legolas Wang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
